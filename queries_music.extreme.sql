3d2580afc08bc9f0f455466db06d7496d3d050c6
janira9345-fuente <janira9345@gmail.com>
2026-03-03 14:00:17 +0100

    archivosql

---
+++ queries_music.extreme.sql
@@ -0,0 +1,47 @@
+
+
+
+
+
+USE music_db;
+
+
+
+CREATE TABLE genre (
+    id_genre INT AUTO_INCREMENT,
+    genre_name VARCHAR(45) NOT NULL,
+    PRIMARY KEY (id_genre)
+);
+
+
+
+CREATE TABLE artists (
+    id_artist INT AUTO_INCREMENT,
+    artist_name VARCHAR(45) NOT NULL, -- 
+    artist_bio LONGTEXT,
+    artist_listeners INT,
+    artist_playcount INT,
+    similar_artist VARCHAR(45),
+    PRIMARY KEY (id_artist)
+);
+
+
+
+CREATE TABLE tracks (
+    id_track INT AUTO_INCREMENT,
+    id_artist INT NOT NULL,     -- 
+    id_genre INT NOT NULL,
+    track_name VARCHAR(45) NOT NULL,
+    year YEAR,
+    PRIMARY KEY (id_track),
+
+    CONSTRAINT fk_tracks_artist
+        FOREIGN KEY (id_artist)
+        REFERENCES artists(id_artist),
+	CONSTRAINT fk_tracks_genre
+        FOREIGN KEY(id_genre)
+        REFERENCES genre(id_genre)
+        );
+    
+        
+        
